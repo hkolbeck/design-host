f941e6c667b28d709c15cb156f43dba2480f6bee
Hannah Kolbeck <hskolbeck@gmail.com>
2022-08-24 20:51:57 -0700

    Whoops, message isn't a function

--- src/gcs.js
+++ src/gcs.js
@@ -46,7 +46,7 @@ function makeFetchPath(bucket) {
         const metadataP = file.getMetadata()
         const [contents, metadataArr] = await Promise.all([contentsP, metadataP])
             .catch(err => {
-                console.log(`Error fetching single for ${path}: ${err.message()}`)
+                console.log(`Error fetching single for ${path}: ${err.message}`)
                 return [null, null]
             })
 
