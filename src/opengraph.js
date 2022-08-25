0bc756de138ad13c04070b7985dad5b9a2ae0f0d
Hannah Kolbeck <hskolbeck@gmail.com>
2022-08-25 12:19:40 -0700

    Get SVG preview working

--- src/opengraph.js
+++ src/opengraph.js
@@ -1,4 +1,5 @@
-// import {createCanvas} from "canvas";
+const {fabric} = require("fabric")
+const fs = require("fs");
 
 async function generateOpengraph(gcs, gcsPath) {
     const metadata = await gcs.getMetadata(gcsPath)
@@ -36,12 +37,82 @@ async function generatePreviewImage(gcs, gcsPath) {
         return {}
     }
 
+    return getPreviewBuffer(mime, contents)
+}
+
+async function getPreviewBuffer(mime, contents) {
     if (mime === "image/png" || mime === "image/jpg" || mime === "image/jpeg") {
         return {mime, contents}
+    } else if (mime === "application/pdf") {
+        const imgContents = await generatePdfPreview(contents)
+        return {mime, contents: imgContents}
+    } else if (mime === "image/svg+xml") {
+        const imgContents = await generateSvgPreview(contents)
+        return {mime, contents: imgContents}
     }
 
     throw new Error(`Unsupported mime type: ${mime}`)
+
 }
+
+async function generatePdfPreview(pdfBuffer) {
+
+}
+
+async function generateSvgPreview(svgBuffer) {
+    const canvas = await new Promise((resolve, reject) => {
+        const workingCanvas = new fabric.Canvas('canvas')
+
+        const bg = new fabric.Rect({
+            width: 2048,
+            height: 2048,
+            left: 0,
+            top: 0,
+            fill: '#FFFFFF'
+        })
+        workingCanvas.add(bg)
+
+        fabric.loadSVGFromString(svgBuffer.toString("utf8"), (curves) => {
+            const grouped = fabric.util.groupSVGElements(curves)
+
+            // const scaleX = 2048 / grouped.getScaledWidth()
+
+            grouped.set({
+                left: 20,
+                top: 20,
+                scaleX: 8,
+                scaleY: 8
+            })
+            workingCanvas.setWidth(grouped.getScaledWidth() + 40)
+            workingCanvas.setHeight(grouped.getScaledHeight() + 40)
+            workingCanvas.add(grouped).renderAll()
+            resolve(workingCanvas)
+        })
+
+            // curves.forEach(curve => {
+            //     // console.log(`Setting ${origHeight} ${origWidth}`)
+            //     curve.set({
+            //         top: 90,
+            //         left: 90,
+            //         originX: 'center',
+            //         originY: 'center'
+            //     });
+            //     curve.scaleToWidth(100);
+            //     curve.scaleToHeight(100);
+            //     workingCanvas.add(curve)
+            // })
+    })
+
+    return fabric.util.getNodeCanvas(canvas.lowerCanvasEl).toBuffer()
+}
+
+async function testPreview(file, mime) {
+    let buffer = fs.readFileSync(file);
+    const outBuffer = (await getPreviewBuffer(mime, buffer)).contents
+    fs.writeFileSync(file + ".png", outBuffer)
+}
+
+testPreview("../site/img/asl-i-love-you.svg", "image/svg+xml")
 
 exports.generateOpengraph = generateOpengraph;
 exports.generatePreviewImage = generatePreviewImage;
