79de150a975e92c7ef5b21d95f6b55897615b15b
Hannah Kolbeck <hskolbeck@gmail.com>
2022-08-25 10:33:18 -0700

    Include discord in preview user agents

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ const previewBots = {
     "node-fetch": true //For testing
 }
 
+const longBots = ["Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)"]
 fastify.get("/images/:img", (request, reply) => {
     reply.sendFile(request.params["img"], path.join(__dirname, "site", "img"))
 })
@@ -77,9 +78,9 @@ fastify.get("/gallery/*", (request, reply) => {
         const ext = request.url.slice(lastDot)
         if (canPreview[ext] && request.headers["user-agent"]) {
             const userAgent = request.headers["user-agent"].match(/^([\w-]+)/)
-            console.log(`User-Agent: ${JSON.stringify(userAgent)}`)
+            console.log(`User-Agent: ${request.headers["user-agent"]} => ${JSON.stringify(userAgent)}`)
             if (userAgent) {
-                if (previewBots[userAgent[0]]) {
+                if (previewBots[userAgent[0]] || longBots.indexOf(request.headers["user-agent"]) >= 0) {
                     const path = decodeURIComponent(request.url).replace("/gallery/", "")
                     sendingPreview = true
                     generateOpengraph(gcs, path)
