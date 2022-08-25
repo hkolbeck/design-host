809f0722077d73160368bf4999164a4252cd43e7
Hannah Kolbeck <hskolbeck@gmail.com>
2022-08-25 14:06:34 -0700

    Add yet another crawler

--- server.js
+++ server.js
@@ -50,7 +50,8 @@ const previewBots = {
 
 const longBots = [
     "Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)",
-    "Mozilla/5.0 (compatible; redditbot/1.0; +http://www.reddit.com/feedback)"
+    "Mozilla/5.0 (compatible; redditbot/1.0; +http://www.reddit.com/feedback)",
+    "Mozilla/5.0 (compatible; Semanticbot/1.0; +http://sempi.tech/bot.html)"
 ]
 fastify.get("/images/:img", (request, reply) => {
     reply.sendFile(request.params["img"], path.join(__dirname, "site", "img"))
